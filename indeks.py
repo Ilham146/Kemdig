6c04dbabb166a099782c287bfa213417442acbc5
Scaramousyyy <lonelymintt@gmail.com>
2025-06-28 22:12:45 +0800

    add Procfile

--- indeks.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-def index():
-    return 'Hello from Flask on Vercel!'
-
-def handler(environ, start_response):
-    return app(environ, start_response)
